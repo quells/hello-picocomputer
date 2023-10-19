2e35054bbd41e3eb6deae0af275e83cc1c4200a5
Kai Wells <support@kaiwells.me>
2023-10-18 22:18:32 -0700

    keyboard

--- src/hello.c
+++ src/hello.c
@@ -1,46 +1,85 @@
 #include <rp6502.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 
-char get_char() {
-    char c;
-    while (1) {
-        if (!(RIA.ready & RIA_READY_RX_BIT))
-            continue;
-        c = RIA.rx;
-        return c;
-    }
+// XRAM
+#define CANVAS_DATA 0x0000
+#define RULER_DATA 0x1000
+#define CANVAS_CONFIG 0xFF00
+#define RULER_CONFIG 0xFF10
+#define KEYBOARD_INPUT 0xFF20
+#define DEFAULT_PALETTE 0xFFFF
+
+void draw_ruler() {
+    unsigned i = 17*17;
+    RIA.addr0 = RULER_DATA;
+    RIA.step0 = 1;
+    for (; i; i--)
+        RIA.rw0 = 0x00;
+
+    RIA.addr0 = RULER_DATA + 1;
+    RIA.step0 = 1;
+    for (i = 16; i; i--)
+        RIA.rw0 = i - 1;
+
+    RIA.addr0 = RULER_DATA + 17;
+    RIA.step0 = 17;
+    for (i = 16; i; i--)
+        RIA.rw0 = i - 1;
 }
 
-char is_ctrl(char c) {
-    /*
-    backspace == 'ctrl-h'
-    enter == 'ctrl-m'
-    tab == 'ctrl-i'
-    escape = 'ctrl-['
-    delete == 'ctrl-[ 0x7f'
-    left == 'ctrl-[ [ D'
-    right == 'ctrl-[ [ C'
-    */
-    if (c < 0x20) 
-        return 0x60 + c; // 0x01 == 'ctrl-a'
-    return 0;
+void loop() {
+    unsigned char i = 1;
+    unsigned char c;
+    unsigned char m = 0x80;
+    RIA.addr0 = KEYBOARD_INPUT;
+    RIA.step0 = 1;
+    RIA.addr1 = CANVAS_DATA;
+    RIA.step1 = 1;
+
+    c = RIA.rw0;
+    do {
+        RIA.rw1 = (c & m) ? 0xFF : 0x00;
+    } while (m >>= 1);
+
+    m = 0x80;
+    do {
+        c = RIA.rw0;
+        do {
+            RIA.rw1 = (c & m) ? 0xFF : 0x00;
+        } while (m >>= 1);
+        m = 0x80;
+        i++;
+    } while (i < 32);
 }
 
-int main() {
-    char c, ctrl;
-    while (1) {
-        c = get_char();
-        if (ctrl = is_ctrl(c)) {
-            switch (ctrl) {
-            case 'c':
-                return 0;
-            default:
-                printf("%02x (%02x)\n", c, ctrl);
-                continue;
-            }
-        } else {
-            printf("%02x ('%c')\n", c, c);
-        }
-    }
+void main() {
+    xreg_vga_canvas(2);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, x_wrap, false);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, y_wrap, false);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, x_pos_px, 1);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, y_pos_px, 1);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, width_px, 16);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, height_px, 16);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, xram_data_ptr, CANVAS_DATA);
+    xram0_struct_set(CANVAS_CONFIG, vga_mode3_config_t, xram_palette_ptr, DEFAULT_PALETTE);
+
+    xreg_vga_canvas(1);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, x_wrap, false);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, y_wrap, false);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, x_pos_px, 0);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, y_pos_px, 0);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, width_px, 17);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, height_px, 17);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, xram_data_ptr, RULER_DATA);
+    xram0_struct_set(RULER_CONFIG, vga_mode3_config_t, xram_palette_ptr, DEFAULT_PALETTE);
+    draw_ruler();
+
+    xreg_vga_mode(3, 2, RULER_CONFIG, 0);
+    xreg_vga_mode(3, 2, CANVAS_CONFIG, 1);
+
+    xreg_ria_keyboard(KEYBOARD_INPUT);
+    while (1)
+        loop();
 }
